3f1c3fcc0581b2aff2c0e9096eb4b48a54baac20
Laucsonium <lucas.massoni51@gmail.com>
2017-07-10 17:24:56 +0200

    chiffrage fini

--- chiffrage.rb
+++ chiffrage.rb
@@ -1,9 +1,18 @@
 def chiffre_de_cesar (texte, clef)
 	texte.split(//).each do |i|
-		i=i.ord+clef
-		print i.chr
+		if i.downcase.ord <97 || i.downcase.ord>122
+			print i
+		else
+			i=i.downcase.ord+clef
+			if i>122 
+				i-=26
+				print i.chr
+			else
+			print i.chr
+			end
+		end
 	end
 end
 
 
-chiffre_de_cesar("What a string!", 5)
+chiffre_de_cesar("ce Programme Fonctionne", 5)
