5c664dc2ac5fcc6be3b4fcc71c5f83daf9bdbd2f
Laucsonium <lucas.massoni51@gmail.com>
2017-07-10 18:52:27 +0200

    stock picker

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,2 +1,13 @@
-def trader_du_dimanche ([])
+prix =[]
+def trader_du_dimanche (prix)
+	i=prix.count-1
+	until i==-1
+		puts prix[i]-prix[i-1]
 	
+
+
+
+		
+	end
+		
+trader_du_dimanche([1,5,19,42,12,564])
