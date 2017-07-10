4df1b4b89316c2d93a7174386f60545f41f990c0
Laucsonium <lucas.massoni51@gmail.com>
2017-07-10 19:35:26 +0200

    boucle fonctionne

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,13 +1,14 @@
 prix =[]
 def trader_du_dimanche (prix)
 	i=prix.count-1
-	until i==-1
-		puts prix[i]-prix[i-1]
-	
-
-
-
-		
+	until i==0 do
+		n=1
+		until n==prix.count do
+		puts prix[i]-prix[i-n] if prix[i]-prix[i-n]>0
+		n+=1
+		end
+		i-=1
 	end
+end
 		
 trader_du_dimanche([1,5,19,42,12,564])
